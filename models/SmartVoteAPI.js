a6901a2d195669a27af1a9493d3d50eaf5c95e06
zqa <zqa19@hotmail.com>
2015-04-09 11:37:06 +1000

    minor changes in Poll.lll & new SmartVoteAPI.js

--- models/SmartVoteAPI.js
+++ models/SmartVoteAPI.js
@@ -2,58 +2,52 @@
  * The SmartVoteAPI class
  */
 function SmartVoteAPI() {
-    this.handle = function (httpRequest) {
-        Println("in handling");
-        //var url_obj = network.parseURL(http_request);
-        //if (url_obj.error != "") {
-        //	return null;
-        //}
-        var path = httpRequest.URL.Path;
-        Println(path);
-        var pathSplit = path.slice(1).split('/');
-        var method = pathSplit[2];
-        if (method === "getVtNum") {
-            var vtnum = getVtNumFromBlockchain();
-            return {
-                "Status" : 200,
-                "Header" : {},
-                "Body" : vtnum
-            };
-        } else if (method === "addVoting") {
-            Println("in server: adding")
-            var votingName = httpRequest.URL.RawQuery.split('=')[1];
-            Println("in server: " + votingName);
-            var txData = [];
-            txData.push(method);
-            txData.push(votingName);
-            Println(txData);
-            monk.Msg(RootContract, txData);
-            monk.Commit();
-            Println("in server: finished");
-            return null;
-        } else if (method === "getVoting") {
-            var voting = "{" +
-            "\"name\" : \"Default\"," +
-            "\"candidates\" : [\"Micky\", \"McDonald\", \"Goofy\"]" +
-            "}";
-            Println(voting);
-            return {
-                "Status" : 200,
-                "Header" : {},
-                "Body" : voting
-            };
-        }
-    };
-}
 
-function getVtNumFromBlockchain() {
-    //var txData = [];
-    //txData.push("getVtNum");
-    //monk.Msg(RootContract, txData);
-    //monk.Commit();
-    Println(RootContract);
-
-    var sa = monk.StorageAt(RootContract, "0x0");
-    Println(sa);
-    return sa.Data;
-}
+	var dougAddr = RootContract;
+	var plAddr = "";
+	var plfAddr = "";
+	var monkAddr = "";
+	
+	var txData = {};
+	
+	this.createPoll = function(ipfsdata, opnum, opentime, closetime, crtusrname, plname) {
+		Println("Adding file to ipfs.");
+		var hash = writeFile(ipfsdata);
+		if (hash === "") {
+			Println("Error when adding file to ipfs.");
+			return "0x0";
+		}
+		return this.postPid(opnum, hash, opentime, closetime, crtusrname, plname);
+	}
+	
+	this.postPid = function(opnum, hash, opentime, closetime, crtusrname, plname) {
+		var txData = [];
+		txData.push("init");
+		txData.push(opnum);
+		txData.push(hash);
+		txData.push(opentime);
+		txData.push(closetime);
+		txData.push(crtusrname);
+		txData.push(plname);
+		var hash = sendMsg(plAddr, txData);
+		return hash;
+	}
+	
+	this.pollOn = function() {
+		txData.push("setstatus");
+		txData.push(1);
+		var hash = sendMsg(plAddr, txData);
+		return hash;
+	}
+	
+	this.pollOff = function() {
+		txData.push("setstatus");
+		txData.push(0);
+		var hash = sendMsg(plAddr, txData);
+		return hash;
+	}
+	
+	function plnameToAddr(plname) {
+		return esl.kv.Main(plfAddr, sutil.stringToHex("polls"), monkAddr);
+	}
+}£»
