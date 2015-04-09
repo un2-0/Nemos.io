de0db3244c21083824ce1a320529c46bfe0b7158
zqa <zqa19@hotmail.com>
2015-04-09 13:13:06 +1000

    Unfinished SmartVote.js

--- models/SmartVote.js
+++ models/SmartVote.js
@@ -1,20 +1,48 @@
 function SmartVote() {
-    Println("Creating SmartVote.");
 
-    var api = new SmartVoteAPI();
+	var COMMITTING = true;
+	
+	svApi = new SmartVoteAPI();
 
-    this.run = function() {
-        Println("Starting SmartVote.")
+	this.handle = function(httpReq) {
+		Println("Receiving");
+		var urlObj = newtork.parseUrl(httpReq);
+		
+		//Error 400 bad request
+		if (urlObj.error !== "") {
+			return null;
+		}
+		var resp = hFunc(urlObj, httpReq);
+		Println("REPONSE OBJECT: " JSON.stringify(resp));
+		
+		//Weak check, but this is clearly not a valid reponse.
+		if (typeof(resp) !== "object" || resp.Body === undefined
+				|| resp.Header === undefined || resp.Status === undefined) {
+			return null;
+		}
+		return resp;
+	}
+	
+	function doPoll(method, body) {
+		if (method === "POST") {
+			var nameObj = JSON.parse(body);
+			var hash = 
+			if (COMMITTING) {
+				monk.Commit();
+			}
+			return network.getHttpResponseJSON(hash);
+		}
+	}
+	
+	this.init = function() {
+		svApi.init();
+	}
+};
 
-        // overwrite the new incoming http callback
-        network.incomingHttpCallback = function (request) {
-            return api.handle(request);
-        }
+// Initialization
+var sv = new SmartVote();
+Println("Starting SmartVote");
+sv.init();
 
-        Println("SmartVote started.");
-    }
-
-}
-
-// start the dapp
-new SmartVote().run();
+network.registerIncomingHttpCallback(sv.handle);
+Println("SmartVote Initialized");
