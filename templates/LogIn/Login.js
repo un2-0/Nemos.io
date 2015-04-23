8843bea234448c1d56db15588ce3642e27336a75
Kun Liu <kunliu89@gmail.com>
2015-04-23 15:16:10 +1000

    changed relative path for seveal .html and .js files

---
+++ templates/LogIn/Login.js
@@ -0,0 +1,11 @@
+function jumpToVoterHome() {
+	window.location.href = "./templates/VoterHome/VoterHome.html";
+}
+
+
+function jumpToOrganizerHome() {
+	window.location.href = "../OrganizerHome/OrganizerHome.html";
+}
+
+
+
--- templates/RegistrationResult/RegistrationResult.js
+++ templates/RegistrationResult/RegistrationResult.js
@@ -1,4 +1,4 @@
 
 function jumpToLogin() {
-	window.location.href = "../LogIn/index.html";
+	window.location.href = "../../index.html";
 }
