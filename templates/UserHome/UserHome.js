33e15cedb26e1654538b74e79149d9e86e264f7f
sorgdas <samuelsaxton28@gmail.com>
2015-04-29 17:00:23 +1000

    adjusted Userhome.html and added some js files.

--- templates/UserHome/UserHome.js
+++ templates/UserHome/UserHome.js
@@ -3,10 +3,20 @@
 function loadInformation(){
 	//window.alert("initialing");
 
-	window.alert(sessionStorage.userName);
+	//window.alert(sessionStorage.userName);
 	
 	var elections = document.getElementById("VoterDisplayElection");
 	var out = "";
+
+	if(sessionStorage.userName !== "" && sessionStorage.userName != null){
+		document.getElementById("welcomer").innerHTML = "<span class='glyphicon glyphicon-user padding-right-small' style='position: relative; top: 3px;'></span>"+ sessionStorage.userName +"<i class='fa fa-caret-down'></i>";
+	}else {
+		document.getElementById("welcomer").innerHTML = "<span class='glyphicon glyphicon-user padding-right-small' style='position: relative; top: 3px;'></span>"+"not login " +" <i class='fa fa-caret-down'></i>";
+	}
+	
+	//showDefaultContent();
+	
+	
 	
 	sender.sendAsync("GET", baseUrl+ "/organiserLogin&", null, function(res){ 
 		
@@ -26,10 +36,17 @@ function loadInformation(){
 			window.alert("failed to get elections");
 		}
 	});
-	
+		
+}
+
+function showElections(){
+	document.getElementById("contentContainer").innerHTML = "";
 	
 }
 
+function createPoll(){
+	document.getElementById("contentContainer").innerHTML = "";
+}
 
 
 function jumpToLogin() {
