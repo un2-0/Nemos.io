e3168d869c8524d545f688369190a2b08ae8c878
Qiao Zhao <zqa@ubuntu.(none)>
2015-05-08 01:37:56 +1000

    More accurate name

--- models/SmartVote.js
+++ models/SmartVote.js
@@ -111,53 +111,53 @@ function SmartVote() {
     }
 
     /**
-     * Creating polls in module1.
+     * Creating elections in module1.
      * @param query = {
-     *            "pollName" : 
+     *            "electionName" : 
      *            "organizerName" : 
      *            "openTime" : 13 digits
      *            "closeTime" : 13 digits
-     *            "pollDes" : the description of the poll
-     *            "voterNum" : how many voters in the poll
-     *            "canoptNum" : how many candidates/options in the poll
+     *            "electionDes" : the description of the election
+     *            "voterNum" : how many voters in the election
+     *            "canoptNum" : how many candidates/options in the election
      *            "rulesNum" : a voter can vote for how many candidates/options in one voting
      *            "canOpts": [{candidate1 name:,candidate1 description},{,},{,},] --- an array that contains 
-     *                all the detail information of all candidates/options in the poll. Each slot in the array includes
+     *                all the detail information of all candidates/options in the election. Each slot in the array includes
      *                an object {name:?,description:?}
      *        }
      * @response response = {
-     *               "result": "success" or "pollNameExist"
+     *               "result": "success" or "electionNameExist"
      *               "publicKeys": [{"id":"01", "password":"Doe"},
      *                             {"id":"02", "password":"Smith"},
      *                             {"id":"03", "password":"Jones"}]
      *                             An array that contains all the public id and
-     *                             random password for each voters in the poll.
-     *                             Each slot in the poll should contain a JSON
+     *                             random password for each voters in the election.
+     *                             Each slot in the election should contain a JSON
      *                             object with "id" and "password"
      *           }
      */
     // TODO
     handlers.module1CreatePoll = function (query) {
         /*
-         * Data structure in request "module1CreatePoll":
+         * Data structure in request "module1CreateElection":
          * a stringtified JSON object that contains:
-         *  Data structure of the response of the "module1CreatPoll":
+         *  Data structure of the response of the "module1CreatElection":
                   */
         printQuery(query); // TODO can be removed
 
-        var pollName = query.pollName;
+        var electionName = query.electionName;
         var organizerName = query.organizerName;
-        var openTime = Number(query.openTime);
-        var closeTime = Number(query.closeTime);
-        var pollDes = query.pollDes;
-        var voterNum = Number(query.voterNum);
-        var canoptNum = Number(query.canoptNum);
-        var rulesNum = Number(query.rulesNum);
+        var openTime = parseInt(query.openTime, 10);
+        var closeTime = parseInt(query.closeTime, 10);
+        var electionDes = query.electionDes;
+        var voterNum = parseInt(query.voterNum, 10);
+        var canoptNum = parseInt(query.canoptNum, 10);
+        var rulesNum = parseInt(query.rulesNum, 10);
         var canOpts = query.canOpts;
         
         var response = {};
-        if (pollNameExists(pollName)) {
-            response.result = "pollNameExist";
+        if (electionNameExists(electionName)) {
+            response.result = "electionNameExist";
             response.publicKeys = null;
             return network.getHttpResponseJSON(JSON.stringify(response));
         }
@@ -192,46 +192,46 @@ function SmartVote() {
     }
 
     /**
-     * Get all the basic information about a particular poll/election.
+     * Get all the basic information about a particular election/election.
      * @param query = {
-     *     "selectedPollName" : str
+     *     "selectedElectionName" : str
      * }
      * @response response = {
      *     "result":"success" | "fail"
-     *     "pollBasicInfo":{
-     *         "pollName": str, 
+     *     "electionBasicInfo":{
+     *         "electionName": str, 
      *         "organizerName": str,
      *         "openTime":
      *         "closeTime":
-     *         "pollDes":
+     *         "electionDes":
      *      }
      * }
      */
     handlers.showPollBasicInfo = function (query) {
         printQuery(query);
-        var pollName = query.selectedPollName;
+        var electionName = query.selectedElectionName;
         var response = {};
-        if (!pollNameExists(pollName)) {
+        if (!electionNameExists(electionName)) {
             response.result = "fail";
-            response.pollBasicInfo = null;
+            response.electionBasicInfo = null;
             return network.getHttpResponseJSON(JSON.stringify(response));
         }
         // TODO
         response.result = "success";
-        response.pollBasicInfo = {};
-        response.pollBasicInfo.pollName = pollName;
+        response.electionBasicInfo = {};
+        response.electionBasicInfo.electionName = electionName;
         return network.getHttpResponseJSON(JSON.stringify(response));
     }
     
     /**
-     * Show all the polls that:
+     * Show all the elections that:
      * 1. are created by the user (if an organizer)
      * 2. the user can participate (if a voter)
      * @param query = {"username" : str, "identity" : str}
      * 
      * @response response = {
      *               "result": "success" or others
-     *               "pollslist": ["pollname1","pollname2","pollname3", ...]
+     *               "electionslist": ["electionname1","electionname2","electionname3", ...]
      *           }
      */
     handlers.showPollList = function (query) {
@@ -241,11 +241,11 @@ function SmartVote() {
         var identity = query.identity;
         if (identity === "organizer" && organizerExist(username)) {
             // TODO
-            // get all the polls that are created by the organizer
+            // get all the elections that are created by the organizer
             // set up response
         } else if (identity === "voter" && voterExist(username)) {
             // TODO
-            // get all the polls that the voter can participate in
+            // get all the elections that the voter can participate in
             // set up response
         } else {
             return network.getHttpResponse(400, {},
@@ -256,8 +256,8 @@ function SmartVote() {
     
     /**
      * Check if the voter has already get the second account for a particular
-     * poll.
-     * @param query = {"username": str, "pollName": str}
+     * election.
+     * @param query = {"username": str, "electionName": str}
      * @response response = {
      *               "result" : "secondPasswordSet" or "secondtPasswordNotSet"
      *                           or "voterNotInList"
@@ -266,7 +266,7 @@ function SmartVote() {
     handlers.checkVoterSecondAccount = function (query) {
         printQuery(query);
         var username = query.username;
-        var pollName = query.pollName;
+        var electionName = query.electionName;
         // TODO
     }
     
@@ -276,7 +276,7 @@ function SmartVote() {
      *            "username" : str
      *            "secondId" : str
      *            "secondPassword" : str
-     *            "selectedPollName" : str
+     *            "selectedElectionName" : str
      *        }
      * @response response = {
      *               "result" : "success" or "userDoesntExist" or
@@ -288,7 +288,7 @@ function SmartVote() {
         var username = query.username;
         var secondId = query.secondId;
         var secondPassword = query.secondPassword;
-        var pollName = query.selectedPollName;
+        var electionName = query.selectedElectionName;
         
         var response = {};
         // TODO
@@ -298,7 +298,7 @@ function SmartVote() {
      * Generate a username-password pair as the second account of a voter.
      * @param query = {
      *     "username" : str,
-     *     "selectedPollName": str
+     *     "selectedElectionName": str
      * }
      * @response response = {
      *     "result" : "success",
@@ -308,13 +308,13 @@ function SmartVote() {
     handlers.getSecondIDPassword = function (query) {
         printQuery(query);
         var firstID = query.username;
-        var pollName = query.selectedPollName;
+        var electionName = query.selectedElectionName;
         
         var response = {};
         response.result = "success";
         var pair = {};
-        pair.id = generateSecondID(pollName);
-        pair.password = generateSecondPassword(firstID, pollName);
+        pair.id = generateSecondID(electionName);
+        pair.password = generateSecondPassword(firstID, electionName);
         response.secondIDPassword = pair;
 
         return network.getHttpResponseJSON(JSON.stringify(response));
@@ -324,7 +324,7 @@ function SmartVote() {
      * Check the ballot by a second ID.
      * @param query = {
      *     "secondId": "The Second ID: ",
-     *     "selectedPollName": "Poll Name: "
+     *     "selectedElectionName": "Election Name: "
      * }
      * @response response = {
      *     "rulesNum": "1",
@@ -336,7 +336,7 @@ function SmartVote() {
     handlers.getVotingInfo = function (query) {
         printQuery(query);
         var secondID = query.secondId;
-        var pollName = query.selectedPollName;
+        var electionName = query.selectedElectionName;
         // TODO
     }
 
@@ -413,16 +413,16 @@ function SmartVote() {
         // TODO
     }
     
-    function generateSecondID(pollName) {
+    function generateSecondID(electionName) {
         // TODO
     }
     
-    function generateSecondPassword(firstID, pollName) {
+    function generateSecondPassword(firstID, electionName) {
         // TODO
     }
 
 	// functions talking with the blockchain
-    function pollNameExists(pollName) {
+    function electionNameExists(electionName) {
         // TODO
         return true;
     }
@@ -492,28 +492,28 @@ function SmartVote() {
 
 
 	this.test = function() {
-		var plAddr = svApi.createPollAccount("myFirstPoll");
+		var plAddr = svApi.createElectionAccount("myFirstElection");
 		if (COMMITTING) {
 			monk.Commit();
 		}
-		Println("8888888888888888888\n" + sutil.hexToString(svApi.test1("myFirstPoll")));
+		Println("8888888888888888888\n" + sutil.hexToString(svApi.test1("myFirstElection")));
 	}
 
 	this.test1 = function() {
-		var plAddr = svApi.createPollAccount("mySecondPoll");
+		var plAddr = svApi.createElectionAccount("mySecondElection");
 		if (COMMITTING) {
 			monk.Commit();
 		}
-		Println("9999999999999999999\n" + sutil.hexToString(svApi.test1("mySecondPoll")));
+		Println("9999999999999999999\n" + sutil.hexToString(svApi.test1("mySecondElection")));
 	}
 
 	this.test2 = function() {
-		var plname = "myFirstPoll";
-		svApi.createPollAccount(plname);
+		var plname = "myFirstElection";
+		svApi.createElectionAccount(plname);
 		if (COMMITTING) {
 			monk.Commit();
 		}
-		svApi.initPoll(plname, 20, "2222", "3333", "444444" ,"55555555", "6666666666");
+		svApi.initElection(plname, 20, "2222", "3333", "444444" ,"55555555", "6666666666");
 		if (COMMITTING) {
 			monk.Commit();
 		}
