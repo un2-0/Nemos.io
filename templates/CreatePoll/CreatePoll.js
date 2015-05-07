725610e4099eae2c90e0e23873e4067e67eb48a6
Kun Liu <kunliu89@gmail.com>
2015-05-07 13:23:13 +1000

    Move all the scripts in html to responding .js files.

--- templates/CreatePoll/CreatePoll.js
+++ templates/CreatePoll/CreatePoll.js
@@ -126,4 +126,33 @@ function showVotings() {
     document.body.appendChild(np);
     container.insertBefore(np, container.fisrtChild);
 */
+    
+//the scripts below were from responding html files    
+    $(function() {
+		var match = document.cookie.match(new RegExp('color=([^;]+)'));
+		if (match)
+			var color = match[1];
+		if (color) {
+			$('body').removeClass(function(index, css) {
+				return (css.match(/\btheme-\S+/g) || []).join(' ')
+			})
+			$('body').addClass('theme-' + color);
+		}
+		$('[data-popover="true"]').popover({
+			html : true
+		});
+	});
+    
+    $(function() {
+		var uls = $('.sidebar-nav > ul > *').clone();
+		uls.addClass('visible-xs');
+		$('#main-menu').append(uls.clone());
+	});
+    
+    $("[rel=tooltip]").tooltip();
+	$(function() {
+		$('.demo-cancel-click').click(function() {
+			return false;
+		});
+	});
 }
--- templates/Registration/Registration.js
+++ templates/Registration/Registration.js
@@ -76,3 +76,34 @@ function matchPassword() {
   alert("You entered a valid password: " + form.passwordone.value);
   return true;
 }
+
+//the scripts below were moved from Registration.html
+$(function() {
+	var match = document.cookie.match(new RegExp('color=([^;]+)'));
+	if (match)
+		var color = match[1];
+	if (color) {
+		$('body').removeClass(function(index, css) {
+			return (css.match(/\btheme-\S+/g) || []).join(' ')
+		})
+		$('body').addClass('theme-' + color);
+	}
+
+	$('[data-popover="true"]').popover({
+		html : true
+	});
+
+});
+
+$(function() {
+	var uls = $('.sidebar-nav > ul > *').clone();
+	uls.addClass('visible-xs');
+	$('#main-menu').append(uls.clone());
+});
+
+$("[rel=tooltip]").tooltip();
+$(function() {
+	$('.demo-cancel-click').click(function() {
+		return false;
+	});
+});
